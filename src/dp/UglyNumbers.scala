16c6ce117aee5293fb1da93faab486e361375abe
rajeevprasanna <rajeevprasanna@gmail.com>
2014-03-24 16:48:48 +0530

    adding scala example for ugly number problem

--- src/dp/UglyNumbers.scala
+++ src/dp/UglyNumbers.scala
@@ -12,6 +12,9 @@ object UglyNumbers extends App {
   var uglyNumbers = List(1)
   val n = 11
 
+  
+  //#Approach1 => implemented similar to java code
+  
   var i2 = 0;
   var i3 = 0;
   var i5 = 0;
@@ -49,4 +52,52 @@ object UglyNumbers extends App {
     case _ if (b <= a && b <= c) => b
     case _ => c
   }
+  
+  
+  println
+  
+  
+   //Approach 2
+  def nums:Stream[Int]={
+    def go(a:Int, b:Int, c:Int):Stream[Int]={
+      val xs = nums.take(a.max(b.max(c))).toArray
+      val a2 = 2 *xs(a-1)
+      val b3 = 3*xs(b-1)
+      val c5 = 5*xs(c-1)
+      if (a2 <= b3 && a2 <= c5) a2 #:: go(a + 1, b, c)
+      else if (b3 <= a2 && b3 <= c5) b3 #:: go(a, b + 1, c)
+      else c5 #:: go(a, b, c + 1)
+    }
+    (1 #:: go(1, 1, 1)).distinct
+  }
+  println(nums.take(n).toList)
+  
+  
+  //Approach3
+  lazy val ugly:Stream[Int] = 1#::Stream.from(2).filter{ n => 
+    ugly.takeWhile(n/2>=).flatten(x => Seq(2,3,5).map(x*)).contains(n)
+  }
+  println(nums.take(n).toList)
+  
+  
+  //Approach4 => Checking for each number and deciding whether it is ugly number or not using brute force approach
+  val primeFactors = Array(2,3,5)
+  def uglyNumber(n: Int): Boolean = n match {
+    case _ if n == 1 => true
+    case _ if n % 5 == 0 => uglyNumber(n / 5)
+    case _ if n % 3 == 0 => uglyNumber(n / 3)
+    case _ if n % 2 == 0 => uglyNumber(n / 2)
+    case _ => false
+  }
+
+  def uglynumbers: Stream[Int] = {
+    def go(x: Int): Stream[Int] =
+      if (uglyNumber(x)) x #:: go(x + 1)
+      else go(x + 1)
+
+    go(1)
+  }
+  println(uglyNumbers.take(n).toList.sorted)
+   
+  
 }
